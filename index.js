351b3fca08dccb3393a0115680ceec27deea459d
Dhruv Aggarwal <aggarwald716@gmail.com>
2023-10-24 18:54:36 +0530

    init

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const plagform = document.getElementById('plagiarismForm');
+const plagiarismForm = document.getElementById('plagiarismForm');
 
-
-plagform.addEventListener('submit', function(event){
+plagiarismForm.addEventListener('submit', function (event) {
     event.preventDefault();
 
     const form = this;
@@ -12,5 +11,19 @@ plagform.addEventListener('submit', function(event){
     const result = textToCheck.toLowerCase().includes('plagiarized content') ? 'Plagiarism detected' : 'No plagiarism detected';
 
     // Display the result
-    const resultContainer = document.getElementById('')
-})
+    const resultContainer = document.getElementById('result');
+    const plagiarismResult = document.getElementById('plagiarismResult');
+
+    // Show the result container
+    resultContainer.classList.remove('hidden');
+
+});
+
+function hideResult() {
+    const resultContainer = document.getElementById('result');
+    resultContainer.classList.add('hidden');
+}
+
+// Hide the result on the form input change
+document.getElementById('file').addEventListener('input', hideResult);
+document.getElementById('content').addEventListener('input', hideResult);
