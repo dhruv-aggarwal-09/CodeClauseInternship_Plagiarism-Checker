0d5e65c0c6df369ce5c4744cbc8d31048e8eac98
Dhruv Aggarwal <aggarwald716@gmail.com>
2023-10-28 00:05:35 +0530

    init

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
-const plagiarismForm = document.getElementById('plagiarismForm');
+// script.js
 
-plagiarismForm.addEventListener('submit', function (event) {
-    event.preventDefault();
+document.getElementById('checkButton').addEventListener('click', checkPlagiarism);
 
-    const form = this;
-    const formData = new FormData(form);
+function checkPlagiarism() {
+    const textToCheck = document.getElementById('content').value;
+    const referenceText = "This is a sample reference text to check for plagiarism.";
 
-    // Simulate a basic plagiarism check (we can replace this with our own logic)
-    const textToCheck = formData.get('content') || '';
-    const result = textToCheck.toLowerCase().includes('plagiarized content') ? 'Plagiarism detected' : 'No plagiarism detected';
+    // Function to calculate Jaccard Similarity
+    function jaccardSimilarity(text1, text2) {
+        const set1 = new Set(text1.split(' '));
+        const set2 = new Set(text2.split(' '));
 
-    // Display the result
-    const resultContainer = document.getElementById('result');
+        const intersection = new Set([...set1].filter(x => set2.has(x)));
+        const union = new Set([...set1, ...set2]);
+
+        return (intersection.size / union.size) * 100;
+    }
+
+    const similarityPercentage = jaccardSimilarity(textToCheck, referenceText);
+
     const plagiarismResult = document.getElementById('plagiarismResult');
+    const resultContainer = document.getElementById('result');
 
-    // Show the result container
+    if (similarityPercentage >= 40) {
+        plagiarismResult.textContent = `Plagiarism detected! Similarity: ${similarityPercentage.toFixed(2)}%`;
+    } else {
+        plagiarismResult.textContent = `No plagiarism detected. Similarity: ${similarityPercentage.toFixed(2)}%`;
+    }
+
     resultContainer.classList.remove('hidden');
-
-});
+}
 
 function hideResult() {
     const resultContainer = document.getElementById('result');
     resultContainer.classList.add('hidden');
 }
 
-// Hide the result on the form input change
-document.getElementById('file').addEventListener('input', hideResult);
+// Hide the result on text input change
 document.getElementById('content').addEventListener('input', hideResult);
+
