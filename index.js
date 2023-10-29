34041ab537b946c209efcc5e76fd9dad3499a0b4
Dhruv Aggarwal <aggarwald716@gmail.com>
2023-10-29 17:56:59 +0530

    init

--- index.js
+++ index.js
@@ -1,55 +1,55 @@
-// Import Axios library
-const axios = require('axios');
+const textInput = document.getElementById("content")
+const plagiarismResult = document.getElementById("plagiarismResult")
+const loaderDiv = document.getElementById("loaderDiv")
+const fileInput = document.getElementById("fileInput")
 
-// Add an event listener to trigger plagiarism check
-document.getElementById('checkButton').addEventListener('click', checkPlagiarism);
 
-// Function to check plagiarism with the API
-async function checkPlagiarism() {
-    // Get the text to check from the textarea
-    const textToCheck = document.getElementById('content').value;
-
-    // Define the API request options
-    const apiOptions = {
-        method: 'POST',
-        url: '',
+function checkPlagiarism() {
+    plagiarismResult.innerText = `--`
+    loaderDiv.classList.remove("hidden")
+    $.ajax({
+        type: "POST",
+        url: "https://plagiarism-checker-and-auto-citation-generator-multi-lingual.p.rapidapi.com/plagiarism",
         headers: {
-            'content-type': '',
-            'X-RapidAPI-Key': '', // Replace with your RapidAPI key
-            'X-RapidAPI-Host': '',
+            "content-type": "application/json",
+            "X-RapidAPI-Key": "bf1b57eae3msh1e94019291e6e47p1dba65jsndab9088dbea4",
+            "X-RapidAPI-Host": "plagiarism-checker-and-auto-citation-generator-multi-lingual.p.rapidapi.com",
         },
-        data: {
-            text: textToCheck,
+        data: JSON.stringify({
+            text: textInput.value.trim(),
+            language: "en",
+            includeCitations: false,
+            scrapeSources: false,
+        }),
+        success: function (data) {
+            loaderDiv.classList.add("hidden")
+            plagiarismResult.innerText = `Plagiarism: ${data['percentPlagiarism']}%`
         },
-    };
-
-    // Make the API request using Axios
-    try {
-        const response = await axios.request(apiOptions);
-
-        // Handle the API response here
-        const plagiarismResult = document.getElementById('plagiarismResult');
-        const resultContainer = document.getElementById('result');
-        const apiResult = response.data;
-        console.log(response.data);
-
-        if (apiResult.someCondition) {
-            plagiarismResult.textContent = `Plagiarism detected! Similarity: ${apiResult.similarity.toFixed(2)}%`;
-        } else {
-            plagiarismResult.textContent = `No plagiarism detected. Similarity: ${apiResult.similarity.toFixed(2)}%`;
+        error: function (error) {
+            loaderDiv.classList.add("hidden")
+            alert("Error Occured !")
+            // console.error(error);
         }
+    });
+}
 
-        resultContainer.classList.remove('hidden');
-    } catch (error) {
-        console.error('Error:', error);
+function openReadFile(){
+    fileInput.click()
+}
+fileInput.addEventListener("change", function () {
+    const selectedFile = fileInput.files[0];
+    if (selectedFile) {
+        const reader = new FileReader();
+
+        reader.onload = function (event) {
+            const fileContent = event.target.result;
+            textInput.textContent = fileContent;
+        };
+
+        reader.readAsText(selectedFile);
+    } else {
+        textInput.textContent = "No file selected";
     }
-}
+});
 
-// Function to hide the result
-function hideResult() {
-    const resultContainer = document.getElementById('result');
-    resultContainer.classList.add('hidden');
-}
-
-// Add an event listener to hide the result on text input change
-document.getElementById('content').addEventListener('input', hideResult);
+// checkPlagiarism()
